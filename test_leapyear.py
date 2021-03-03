8c8231352d57deae2744c8461a983f10be9754a4
radfordm-osu <77699896+radfordm-osu@users.noreply.github.com>
2021-03-02 22:08:38 -0800

    wrote test 2 for leapyear
    Sends 200, and expects False.

--- test_leapyear.py
+++ test_leapyear.py
@@ -6,5 +6,9 @@ class leapyeartester(unittest.TestCase):
         result = leapyear.leapyear(204)
         self.assertEqual(result, True)
 
+    def test_leapyear_200(self):
+        result = leapyear.leapyear(200)
+        self.assertEqual(result, False)
+
     if __name__ == "__main__":
         unittest.main()
