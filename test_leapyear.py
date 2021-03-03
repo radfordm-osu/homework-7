5a5fbc7aec2b7f202552353e8ffb35055d5ca64b
radfordm-osu <77699896+radfordm-osu@users.noreply.github.com>
2021-03-02 22:14:10 -0800

    wrote test 4 for leapyear
    this test sends 410, which is not a leap year. False is expected.

--- test_leapyear.py
+++ test_leapyear.py
@@ -13,6 +13,10 @@ class leapyeartester(unittest.TestCase):
     def test_leapyear_400(self):
         result = leapyear.leapyear(400)
         self.assertEqual(result, True)
+        
+    def test_leapyear_410(self):
+        result = leapyear.leapyear(410)
+        self.assertEqual(result, False)
 
     if __name__ == "__main__":
         unittest.main()
